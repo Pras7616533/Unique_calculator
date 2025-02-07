3936f40ab700c99e4e32ce6b53392c5b91170c2f
Prashant Deshmukh <143571194+Pras7616533@users.noreply.github.com>
2025-02-07 12:33:39 +0530

    Update db.py

--- Python/db.py
+++ Python/db.py
@@ -1,1 +1,12 @@
+def fwrite(m):
+    with open("db.txt", "a") as f:
+        f.write(m)
+        f.write("\n")
+        f.close()
 
+
+def fread():
+    with open("db.txt", "r") as f:
+        s =  f.read()
+        print(s)
+        f.close()
