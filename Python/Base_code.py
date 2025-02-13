2b7596bb628cd3c369c24dc5625cc7b255c9c549
Prashant Deshmukh <143571194+Pras7616533@users.noreply.github.com>
2025-02-13 20:26:54 +0530

    Update Base_code.py

--- Python/Base_code.py
+++ Python/Base_code.py
@@ -1,68 +1,79 @@
 import if_block as main
-import Print_view as mai
+import db
 
 
 def decimal(code=0000):
-  print("                 _-_-_-_-_-Decimal-_-_-_-_-_")
-  mai.print_view()
+  f = "                 _-_-_-_-_-Decimal-_-_-_-_-_"
+  print(f)
+  db.fwrite(f)
   main.if_block(code)
 
 
 def binary(code=1010):
-  print("                 _-_-_-_-_-Binary-_-_-_-_-_")
-  mai.print_view()
+  f = "                 _-_-_-_-_-Binary-_-_-_-_-_"
+  print(f)
+  db.fwrite(f)
   main.if_block(code)
 
 
 def quaternary(code=1212):
-  print("                 _-_-_-_-_-Quaternary-_-_-_-_-_")
-  mai.print_view()
+  f = "                 _-_-_-_-_-Quaternary-_-_-_-_-_"
+  print(f)
+  db.fwrite(f)
   main.if_block(code)
 
 
 def senary(code=1023):
-  print("                 _-_-_-_-_-Senary-_-_-_-_-_")
-  mai.print_view()
+  f = "                 _-_-_-_-_-Senary-_-_-_-_-_"
+  print(f)
+  db.fwrite(f)
   main.if_block(code)
 
 
 def octal(code=1234):
-  print("                 _-_-_-_-_-Octal-_-_-_-_-_")
-  mai.print_view()
+  f = "                 _-_-_-_-_-Octal-_-_-_-_-_"
+  print(f)
+  db.fwrite(f)
   main.if_block(code)
 
 
 def duodecimal(code=1298):
-  print("                 _-_-_-_-_-Duodecimal-_-_-_-_-_")
-  mai.print_view()
+  f = "                 _-_-_-_-_-Duodecimal-_-_-_-_-_"
+  print(f)
+  db.fwrite(f)
   main.if_block(code)
 
 
 def quadrodecimal(code=1462):
-  print("                 _-_-_-_-_-Quadrodecimal-_-_-_-_-_")
-  mai.print_view()
+  f = "                 _-_-_-_-_-Quadrodecimal-_-_-_-_-_"
+  print(f)
+  db.fwrite(f)
   main.if_block(code)
 
 
 def hexadecimal(code=1636):
-  print("                 _-_-_-_-_-Hexadecimal-_-_-_-_-_")
-  mai.print_view()
+  f = "                 _-_-_-_-_-Hexadecimal-_-_-_-_-_"
+  print(f)
+  db.fwrite(f)
   main.if_block(code)
 
 
 def quadravigesimal(code=2412):
-  print("                 _-_-_-_-_-Quadravigesimal-_-_-_-_-_")
-  mai.print_view()
+  f = "                 _-_-_-_-_-Quadravigesimal-_-_-_-_-_"
+  print(f)
+  db.fwrite(f)
   main.if_block(code)
 
 
 def duotrigesimal(code=3268):
-  print("                 _-_-_-_-_-Duotrigesimal-_-_-_-_-_")
-  mai.print_view()
+  f = "                 _-_-_-_-_-Duotrigesimal-_-_-_-_-_"
+  print(f)
+  db.fwrite(f)
   main.if_block(code)
 
 
 def tetrasexagesimal(code=6486):
-  print("                 _-_-_-_-_-Tetrasexagesimal-_-_-_-_-_")
-  mai.print_view()
+  f = "                 _-_-_-_-_-Tetrasexagesimal-_-_-_-_-_"
+  print(f)
+  db.fwrite(f)
   main.if_block(code)
